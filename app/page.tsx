983f7f73edb85685250a46db0eca19e6ba71e388
Daniela Londoño <101213330+daniela03h@users.noreply.github.com>
2024-08-14 16:41:34 -0500

    Create products table in index view (#2)
    Co-authored-by: AarFeE <andres.jimenezv@hotmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+'use client'
+
+import { Table } from "./_components/table";
 
 export default function Home() {
   return (
     <main>
       <h1>Index</h1>
+      <Table/>
     </main>
   );
 }

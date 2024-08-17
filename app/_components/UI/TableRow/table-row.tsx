c624b7ad576f0afa1d9b60889995783637d2b9c7
Kevin Rodas Cuartas <kevin.rodas.cuartas@gmail.com>
2024-08-17 11:57:26 -0500

    create buttons edit and delete with icons

--- app/_components/UI/TableRow/table-row.tsx
+++ app/_components/UI/TableRow/table-row.tsx
@@ -2,13 +2,19 @@ import { ITrProps } from "@/app/_types/types";
 import { TableRow } from '../../product-table/styled';
 import Tc from "../TableCell/table-cell";
 import Image from "../Image/image";
+import IconButtonEdit from "../IconButtons/iconButtonsEdit";
+import IconButtonDelete from "../IconButtons/iconButtonsDelete";
 
 const Tr:React.FC<ITrProps> = ({row,columns}) => {
     return (
         <TableRow>
             {columns.map((col, index) => {
                  if (col === 'image'){
-                    return <Image src={row[col]}/>
+                    return <Image src={row[col]} alt="" key={index}/> 
+                 }
+                 if(col==="actions"){
+                    return [<IconButtonEdit className="bi bi-pencil-square" key={index}/>, <IconButtonDelete className="bi bi-trash3" key={index}/>
+                ]
                  }else{
                     return <Tc key={index} content={row[col]}></Tc>
                  }
